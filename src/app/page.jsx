8f72aa2ea676dfc66c3c0574f211e29add1d0b72
Bushra jabin <bushrabintjabin@gmail.com>
2024-11-06 01:31:08 +0530

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,15 @@
 // src/app/page.jsx
 
-"use client"; // Client Component banane ke liye directive
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Ensure redirect happens only once
-    if (typeof window !== "undefined") {
-      router.replace("/login");
-    }
-  }, [router]);
-
-  return null; // Redirect ke baad kuch render nahi karna
-}
+export async function getServerSideProps() {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+  
+  export default function Home() {
+    return null; // Redirect ke baad kuch render nahi hoga
+  }
+  
