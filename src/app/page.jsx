a35d89ec8e6ec9a82cc8ff6962943ee55fe5bc9b
Bushra jabin <bushrabintjabin@gmail.com>
2024-11-06 01:43:32 +0530

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
- export default function page() {
+ export default function Page() {
   return (
     <div className="bg-green-900 flex flex-row items-center w-44 m-auto text-center justify-center mt-10">
       <Link href="/login">Login page</Link>
