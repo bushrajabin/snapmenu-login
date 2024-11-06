b0347dc57e5031ac1d21ec28e92b9ca5c75fe17a
Bushra jabin <bushrabintjabin@gmail.com>
2024-11-06 19:23:17 +0530

    update classNames

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,46 +5,46 @@ import custom from "./login.module.css";
 
 export default function Login() {
   return (
-    <div className={custom.loginmaindiv}>
+    <div className={custom.loginMainDiv}>
       <div className={custom.logoDiv}>
-        <Image src={Logo} alt="Logo image" className={custom.logoimage} />
+        <Image src={Logo} alt="Logo image" className={custom.logoImage} />
       </div>
 
-      <div className={custom.loginpage}>
-        <div className={custom.loginhead}>Login</div>
-        
+      <div className={custom.loginPage}>
+        <div className={custom.loginHead}>Login</div>
+
         <div className={custom.input}>
           <input
             type="text"
             placeholder="Enter your email"
-            className={custom.inputfield}
+            className={custom.inputField}
           />
           <input
             type="password"
             placeholder="Enter your password"
-            className={custom.inputfield}
+            className={custom.inputField}
           />
         </div>
 
         <div className={custom.buttons}>
-          <div className={custom.loginbutton}>
+          <div className={custom.loginButton}>
             <button>Login</button>
           </div>
 
-          <div className={custom.contnuewith}>
+          <div className={custom.contnueWith}>
             <div className={custom.line}></div>
             <div className="pl-4 pr-4 text-sm text-black">or continue with</div>
             <div className={custom.line}></div>
           </div>
 
-          <div className={custom.googlebtn}>
+          <div className={custom.googleBtn}>
             <button>Google</button>
           </div>
 
-          <div className={custom.signupoption}>
+          <div className={custom.signupOption}>
             <h2 className="text-black">
               Don&apos;t have an account?{" "}
-              <span className="text-green-400">Sign up</span> 
+              <span className="text-green-400">Sign up</span>
             </h2>
           </div>
         </div>
